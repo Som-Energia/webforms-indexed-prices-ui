4da3c5f0ea15af58e19db70253297754c1a658ff
joana <joana.figueira@somenergia.coop>
2024-03-25 16:56:23 +0100

    🐛 move button style to specific button not general

--- src/components/SomenergiaTheme.js
+++ src/components/SomenergiaTheme.js
@@ -22,10 +22,6 @@ const SomenergiaTheme = createTheme({
           textTransform: 'none',
           lineHeight: '1.4em',
           border: 0,
-          '@media (min-width: 600px)': {
-            width: '150px',
-            padding: '0.4em 0em 0.4em 0em',
-          },
         },
       },
     },
