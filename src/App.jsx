146f40b5dd78741de3e1af3f448838e768708d08
marta197 <marta.fernandez@somenergia.coop>
2025-06-25 15:39:35 +0200

    🏛️🍇🎭 all roads lead to indexed prices
    Co-authored-by: dukenuken09 <gilgassorovira@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,26 @@
 import React from 'react'
+import i18n from './i18n/i18n'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
 import { TariffNameContextProvider } from './components/TariffNameContextProvider'
 import IndexedDailyPrices from './pages/IndexedDailyPrices'
-import IndexedHistoricPrices from './pages/IndexedHistoricPrices'
-import TestPage from './pages/TestPage'
 import SomenergiaTheme from './components/SomenergiaTheme'
+
+const urls = [
+  '/:language/tarifes-delectricitat-que-oferim/tarifa-indexada/preu-avui/',
+  '/:language/tarifas-de-electricidad-que-ofrecemos/tarifa-indexada/precio-hoy/',
+  '/:language/tarifas-electricas-que-ofrecemos/tarifa-indexada/prezo-hoxe/',
+  '/:language/eskaintzen-ditugun-elektrizitate-tarifak/tarifa-indexatua/gaurko-prezioa/',
+]
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <TestPage />,
-  },
-  {
-    path: '/:language/indexed-daily-prices',
+    path: '/:language/*',
     element: (
       <TariffNameContextProvider>
         <IndexedDailyPrices />
       </TariffNameContextProvider>
     ),
-  },
-  {
-      path: '/:language/tarifes-delectricitat-que-oferim/tarifa-indexada/preu-avui/',
-    element: (
-      <TariffNameContextProvider>
-        <IndexedDailyPrices />
-      </TariffNameContextProvider>
-    ),
-  },
-  {
-      path: '/:language/tarifas-de-electricidad-que-ofrecemos/tarifa-indexada/precio-hoy/',
-    element: (
-      <TariffNameContextProvider>
-        <IndexedDailyPrices />
-      </TariffNameContextProvider>
-    ),
-  },
-  {
-      path: '/:language/tarifas-electricas-que-ofrecemos/tarifa-indexada/prezo-hoxe/',
-    element: (
-      <TariffNameContextProvider>
-        <IndexedDailyPrices />
-      </TariffNameContextProvider>
-    ),
-  },
-  {
-      path: '/:language/eskaintzen-ditugun-elektrizitate-tarifak/tarifa-indexatua/gaurko-prezioa/',
-    element: (
-      <TariffNameContextProvider>
-        <IndexedDailyPrices />
-      </TariffNameContextProvider>
-    ),
-  },
-  {
-    path: '/:language/indexed-historic-prices',
-    element: <IndexedHistoricPrices />,
   },
 ])
 
